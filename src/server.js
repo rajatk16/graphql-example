af646f103dd77351995906b4b98d577bfafdfba8
rajatk16 <rajat.sudagade@gmail.com>
2020-03-11 17:08:44 -0500

    "graphql basics"

--- src/server.js
+++ src/server.js
@@ -4,20 +4,42 @@ import gql from 'graphql-tag';
 // Type Definitions
 const typeDefs = gql`
   type Query {
-    hey: String!
+    me: User!
+    post: Post!
+    users: [User!]!
+  }
+
+  type User {
+    id: ID!
     name: String!
-    location: String!
-    bio: String!
+    email: String!
+    age: Int!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
   }
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    hey: () => 'this is my first Query',
-    name: () => 'Rajat S',
-    location: () => 'Dallas TX',
-    bio: () => 'Developer at Infovision'
+    me: () => ({
+      id: '64616566',
+      name: 'Rajat',
+      email: 'rajat@test.com',
+      age: 25
+    }),
+    post: () => ({
+      id: '5165651651',
+      title: 'GraphQL Basics',
+      body: 'Learn Basics of GraphQL',
+      published: false
+    }),
+    user: (parent, args, ctx, info) => {}
   }
 };
 
